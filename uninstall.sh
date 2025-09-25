3850cbeb9dde052eefdc5bc4d3e70f18e02c0108
udinz <tamvanzudin@gmail.com>
2025-09-25 23:18:58 +0700

    Add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,21 @@
+# [+] =============================================== [+]
+# [+]                                                 [+]
+# [+] - INSTALLER PTERODACTYL                         [+]
+# [+]                                                 [+]
+# [+]  TQTO :                                         [+]
+# [+] • Gustafhosting ( my friends)                            [+]
+# [+] • Creator Bot WhatsApp & Telegram               [+]
+# [+] • Para Pengguna Bot Tele & Wa                   [+]
+# [+]                                                 [+]
+# [+] © GUSTAFHOSTING                               [+]
+# [+] =============================================== [+]
+
+# Membuat lokasi baru
+bash <(curl -s https://pterodactyl-installer.se) <<EOF
+y
+y
+y
+y
+EOF
+
+echo "Proses Uninstall Panell Selesai."
